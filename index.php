bd934c87b66bd23366f70879d57a1a9aae17eb71
Graphene Petal <60903553+GraphenePetal@users.noreply.github.com>
2020-04-03 00:06:57 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+ <html>
+<body>
+
+ <?php
+echo "This docker compose file is for testing the application<br>";
+echo "you can replace this file by another one but keep the name index.php in order to avoid errors from Apache server or you can configure it as you want, I don't know many details so you can see their website to learn more<br>";
+?> 
+
+</body>
+</html> 
